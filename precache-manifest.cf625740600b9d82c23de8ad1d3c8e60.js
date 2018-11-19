9cb3e32bf986ce2d1a0c5fd10833ee1b3b7dfc16
AnastasiaBro <a-sirion@yandex.ru>
2018-11-20 01:00:29 +0300

    Updates

---
+++ precache-manifest.cf625740600b9d82c23de8ad1d3c8e60.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a82517b99a722748cc0b1fd6461e1b8",
+    "url": "/portfolio/static/media/gulp.1a82517b.png"
+  },
+  {
+    "revision": "d11f8f8c86ae85faec8a",
+    "url": "/portfolio/static/css/main.a122a4ef.chunk.css"
+  },
+  {
+    "revision": "eaec248a692865fbbd50",
+    "url": "/portfolio/static/js/1.eaec248a.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "e58109e587d0d9c974c4f7d6f4b459b2",
+    "url": "/portfolio/static/media/sedona.e58109e5.jpg"
+  },
+  {
+    "revision": "6527dcc65ef7d9ae7228f6918601d641",
+    "url": "/portfolio/static/media/education.6527dcc6.png"
+  },
+  {
+    "revision": "e894c6594be3b6d72f5a31cf76358f71",
+    "url": "/portfolio/static/media/place.e894c659.png"
+  },
+  {
+    "revision": "463307be69e9c01a6840f7e0897014c9",
+    "url": "/portfolio/static/media/pink.463307be.jpg"
+  },
+  {
+    "revision": "a677977ea5fce841ec82ba41e3b63657",
+    "url": "/portfolio/static/media/desktop.a677977e.png"
+  },
+  {
+    "revision": "5252b16ff5ef184c7fc0d50e36792e2c",
+    "url": "/portfolio/static/media/SertificateFirst.5252b16f.JPG"
+  },
+  {
+    "revision": "64132678c297ec39a5e7ca5050136ed1",
+    "url": "/portfolio/static/media/SertificateSecond.64132678.JPG"
+  },
+  {
+    "revision": "273e610201c7e597b99cb1a79e22cdfa",
+    "url": "/portfolio/static/media/SertificateThird.273e6102.JPG"
+  },
+  {
+    "revision": "8ec908094ae2c2c7a8bf1c2532d5d7a8",
+    "url": "/portfolio/static/media/html.8ec90809.png"
+  },
+  {
+    "revision": "1a756eb6ca471177cdbd231745af5885",
+    "url": "/portfolio/static/media/js.1a756eb6.png"
+  },
+  {
+    "revision": "a8c5c5b77ef63ad4a47c4f46b23bede6",
+    "url": "/portfolio/static/media/react.a8c5c5b7.png"
+  },
+  {
+    "revision": "d11f8f8c86ae85faec8a",
+    "url": "/portfolio/static/js/main.d11f8f8c.chunk.js"
+  },
+  {
+    "revision": "27642546c3ca8f7ced4e76ec23930465",
+    "url": "/portfolio/static/media/bem.27642546.svg"
+  },
+  {
+    "revision": "0ba4ded17ed13a1721b1898b8dc86740",
+    "url": "/portfolio/static/media/keksobooking.0ba4ded1.jpg"
+  },
+  {
+    "revision": "049d7f031a0d6c417e76ecfd682a432f",
+    "url": "/portfolio/static/media/pic1.049d7f03.jpg"
+  },
+  {
+    "revision": "6d93cdf7ff40bb1798b7d1b6e2b61da0",
+    "url": "/portfolio/static/media/pic2.6d93cdf7.jpg"
+  },
+  {
+    "revision": "60a68552cd96de0b13729ba4a5154ecd",
+    "url": "/portfolio/static/media/pic3.60a68552.jpg"
+  },
+  {
+    "revision": "30a7786b280b75e3ec0a18ce1d6c9383",
+    "url": "/portfolio/static/media/pic4.30a7786b.jpg"
+  },
+  {
+    "revision": "ee2216bef37be52f5e797e895079785b",
+    "url": "/portfolio/static/media/pic5.ee2216be.jpg"
+  },
+  {
+    "revision": "fd24f3827e1d7dc4847983675b2ca27a",
+    "url": "/portfolio/static/media/pic6.fd24f382.jpg"
+  },
+  {
+    "revision": "f5ee395f8ce5f76477012c66e91c958c",
+    "url": "/portfolio/static/media/pic7.f5ee395f.jpg"
+  },
+  {
+    "revision": "0d8a9c1e19c64b51d0f5c03a0b6ee418",
+    "url": "/portfolio/static/media/pic8.0d8a9c1e.jpg"
+  },
+  {
+    "revision": "daf28ed3d835930e993fe8f9f36c5757",
+    "url": "/portfolio/static/media/pic9.daf28ed3.jpg"
+  },
+  {
+    "revision": "eb3d629beb5abad77394391490a5d183",
+    "url": "/portfolio/static/media/instagram.eb3d629b.png"
+  },
+  {
+    "revision": "99a7959b0961495911c9a68a1bfd5782",
+    "url": "/portfolio/static/media/smart.99a7959b.png"
+  },
+  {
+    "revision": "be883cbc0b12cc57ec7a8fdc6308f2e7",
+    "url": "/portfolio/index.html"
+  }
+];
